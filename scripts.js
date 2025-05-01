f18232f08f56e88d1640b6fa88f007662137f705
Cleriston Melo <cleriston.melo@gmail.com>
2025-05-01 17:49:55 -0300

    validação que o primeiro número tem sempre que ser menor que o segundo para a realização do sorteio

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@ function generateNumbers(){
     const min = Math.ceil(document.querySelector(".min").value)
     const max = Math.floor(document.querySelector(".max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    const message = "O número sorteado foi ";
-    alert(message + result)
+    if(min < max) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        const message = "O número sorteado foi ";
+        alert(message + result)
+    } else {
+        alert("O valor mínimo tem que ser MENOR que o valor máximo")
+    }
 }
